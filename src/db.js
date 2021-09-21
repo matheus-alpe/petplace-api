217174502fa0713788b77b15c4ec65cfddd76b52
Matheus A. Pereira <matttalves@gmail.com>
2021-09-20 22:07:45 -0300

    fix: column order

--- src/db.js
+++ src/db.js
@@ -30,8 +30,8 @@ pool.getConnection((err, connection) => {
         id varchar(255) not null primary key,
         name varchar(255) not null,
         cpf varchar(255) not null,
-        image varchar(255) not null,
         email varchar(255) not null,
+        image varchar(255) not null,
         password varchar(255) not null
     );`)
 
