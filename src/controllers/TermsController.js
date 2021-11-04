219af8ddf0707f7d86d67b68cfd26b404fd98040
Matheus A. Pereira <matttalves@gmail.com>
2021-11-04 20:42:55 -0300

    fix: term controller

--- src/controllers/TermsController.js
+++ src/controllers/TermsController.js
@@ -1,145 +1,45 @@
-import { readFileSync, writeFileSync} from 'fs';
-import { changeOwners, checkPetOwner, checkTermID, createResponsibilityTerm, getPetsByProperty, getPropertyFromPet, getPropertyFromUser, getUserByProperty, updateResponsibilityTerm } from '../db.js';
-
-const path = new URL('../../__mocks__/pets.json', import.meta.url);
-
-
-function validateInputs(responsibilityTerm){
-    return new Promise(async function(resolve){
-        let erros = {};
-        let tempUser = await getUserByProperty(responsibilityTerm.donator_identifier, 'cpf');
-        if (!tempUser) tempUser = await getUserByProperty(responsibilityTerm.donator_identifier, 'cnpj');
-        if (!tempUser) {
-            erros['donator_identifier'] = 'cpf ou cnpj do doador não existe';
-        } 
-        
-
-        if (responsibilityTerm.adopter_identifier){
-            tempUser = await getUserByProperty(responsibilityTerm.adopter_identifier, 'cpf');
-            if (!tempUser) tempUser = await getUserByProperty(responsibilityTerm.adopter_identifier, 'cnpj');
-            if (!tempUser) {
-                erros['adopter_identifier'] = 'cpf ou cnpj do adotador não existe';
-            }
-        }
-        
-
-        let tempPet = await getPetsByProperty(responsibilityTerm.pet_id, 'id');
-        if (!tempPet) {
-            erros['pet_id'] = 'id do pet não existe';
-        }else{
-            tempUser = await checkPetOwner(responsibilityTerm);
-            if(!tempUser){
-                erros['wrong_owner'] = 'pet não pertence ao dono de cpf ou cnpj passado';
-            }
-        }
-
-        if(!responsibilityTerm.status) erros['status_undefined'] = 'o status do termo não foi definido';
-
-        if (erros.donator_identifier || erros.adopter_identifier || erros.pet_id || erros.wrong_owner){
-            resolve(erros);
-        }else{
-            //transforming JSON to string and getting cpf from pet owner then comparing to adopter_cpf
-            tempPet = await getPropertyFromPet('user_id', responsibilityTerm.pet_id);
-            tempPet = JSON.stringify(tempPet);
-            tempPet = tempPet.substring(12, tempPet.length-2 );
-            tempUser = await getPropertyFromUser('cpf', tempPet);
-            if(!tempUser.cpf) {
-                tempUser = await getPropertyFromUser('cnpj', tempPet);
-                tempUser = JSON.stringify(tempUser);
-                tempUser = tempUser.substring(8, tempUser.length-1 );
-            }else{
-                tempUser = JSON.stringify(tempUser);
-                tempUser = tempUser.substring(7, tempUser.length-1 );
-            }
-            
-            if( tempUser == responsibilityTerm.adopter_identifier){
-                erros['owner_is_adopter'] = 'pet pertence ao usuário do cpf ou cnpj que quer adotar';
-            }
-            if(erros.owner_is_adopter) resolve(erros);
-        }
-        
-
-        resolve();
-    });
-}
-
-function validateUpdate(responsibilityTerm){
-    return new Promise(async function(resolve){
-        let erros = {};
-        
-        let tempTerm = await checkTermID(responsibilityTerm.id);
-        if (!tempTerm) erros['id'] = 'id do termo de responsabilidade não existe';
-        
-        if(!responsibilityTerm.status) erros['status_undefined'] = 'o status do termo não foi definido';
-        
-        if( responsibilityTerm.donator_identifier == responsibilityTerm.adopter_identifier) erros['owner_is_adopter'] = 'pet pertence ao usuário do cpf ou cnpj que quer adotar';
-        
-        if (responsibilityTerm.adopter_identifier){
-            let tempUser = await getUserByProperty(responsibilityTerm.adopter_identifier, 'cpf');
-            if (!tempUser) tempUser = await getUserByProperty(responsibilityTerm.adopter_identifier, 'cnpj');
-            if (!tempUser) {
-                erros['adopter_identifier'] = 'cpf ou cnpj do adotador não existe';
-            }
-        }
-
-        if(erros.id || erros.status_undefined || erros.owner_is_adopter) resolve(erros);
-        
-        
-        resolve();
-    });
-}
-
-function identifierToID(value){
-    return new Promise(async function(resolve){
-        let tempUser = await getUserByProperty(value,'cpf');
-        if(!tempUser) tempUser = await getUserByProperty(value, 'cnpj');
-
-        resolve(tempUser);
-    });
-}
+import { createResponsibilityTerm, updateResponsibilityTerm, archiveTerms } from '../db.js';
 
 export default {
     
     async create(req,res){
         if(res.statusCode == 200){
+            try {
+                const { responsibilityTerm } = req.body;
+                responsibilityTerm.id = new Date().toISOString().replace(/[^\w\s]/gi, '');
 
-            const { responsibilityTerm } = req.body;
-            responsibilityTerm.id = new Date().toISOString().replace(/[^\w\s]/gi, '');
-
-            const erros = await validateInputs(responsibilityTerm);
-            if(erros) return res.status(403).send({erros});
-
-
-            await createResponsibilityTerm(responsibilityTerm);
-            res.status(200).send({ responsibilityTerm });
-        }
-    },
-
-    async exchange(req,res){
-        if(res.statusCode == 200){
-            const { responsibilityTerm } = req.body;
-
-            const erros = await validateInputs(responsibilityTerm);
-            if(erros) return res.status(403).send({erros});
-
-            let adopter = await identifierToID(responsibilityTerm.adopter_identifier); 
-            let donator = await identifierToID(responsibilityTerm.donator_identifier);
-
-            await changeOwners(responsibilityTerm.pet_id, adopter, donator);
-            res.status(200).send({ ok: true });
+                await createResponsibilityTerm(responsibilityTerm);
+                res.status(200).send({ responsibilityTerm });
+            } catch (error) {
+                res.status(404).send({ error });
+            }
         }
     },
 
     async update(req,res){
         if(res.statusCode == 200){
-            const { responsibilityTerm } = req.body;
+            try {
+                const { responsibilityTerm } = req.body;
 
-            let erros = await validateUpdate(responsibilityTerm);
-            if (erros) return res.status(403).send({erros});
-
-            await updateResponsibilityTerm(responsibilityTerm);
-            res.status(200).send({ ok: true });
+                await updateResponsibilityTerm(responsibilityTerm);
+                res.status(200).send({ ok: true, responsibilityTerm });
+            } catch (error) {
+                res.status(404).send({ error });
+            }
         }        
-    }
+    },
+
+    async archive(req,res){
+        if(res.statusCode == 200){
+            try {
+                const { responsibilityTerm } = req.body;
+
+                await archiveTerms(responsibilityTerm);
+                res.status(200).send({ ok: true });
+            } catch (error) {
+                res.status(404).send({ error });
+            }
+        }
+    },
     
 }
